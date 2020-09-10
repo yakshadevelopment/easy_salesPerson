8a999639b5362fd572e756f3d2cf75f1d3395cee
Administrator <Administrator@sumedha-PC>
2020-09-10 17:52:36 +0530

    Updated Uploaded again

--- easyapp2a/src/main/java/java9/easy/easyapp2a/SalesPerson.java
+++ easyapp2a/src/main/java/java9/easy/easyapp2a/SalesPerson.java
@@ -1,13 +1,5 @@
 package java9.easy.easyapp2a;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class SalesPerson implements Comparable<SalesPerson>{
 	private Integer id;
 	private String name;
@@ -16,6 +8,61 @@ public class SalesPerson implements Comparable<SalesPerson>{
 	
 	
 	
+	public SalesPerson() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public SalesPerson(Integer id, String name, Integer saleAmount, String grade) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.saleAmount = saleAmount;
+		this.grade = grade;
+	}
+    
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Integer getSaleAmount() {
+		return saleAmount;
+	}
+
+
+	public void setSaleAmount(Integer saleAmount) {
+		this.saleAmount = saleAmount;
+	}
+
+
+	public String getGrade() {
+		return grade;
+	}
+
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+
 	@Override
 	public String toString() {
 	 String output=String.format("%-5s %-20s %-10s %-10s",id,name,saleAmount,grade);
